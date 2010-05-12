dd8055e592495f144bd9bb819b0316510325681f
Steven Haryanto <stevenharyanto@gmail.com>
2010-05-12 18:00:42 +0000

    Import of SHARYANTO/App-perlmv-0.24 from CPAN.
    gitpan-cpan-distribution: App-perlmv
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         SHARYANTO/App-perlmv-0.24.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/perlmv.pm
@@ -0,0 +1,32 @@
+package App::perlmv;
+BEGIN {
+  $App::perlmv::VERSION = '0.24';
+}
+# ABSTRACT: Rename files using Perl code.
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::perlmv - Rename files using Perl code.
+
+=head1 VERSION
+
+version 0.24
+
+=head1 AUTHOR
+
+  Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
